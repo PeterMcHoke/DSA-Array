48d97f39975d1724b3e4a5eefde3754470f3aa67
PeterMcHoke <petermchoke@gmail.com>
2021-01-28 11:47:39 -0600

    Finished Array class implementation 😎 and array drills ✅

---
+++ array.js
@@ -0,0 +1,77 @@
+const Memory = require('./memory')
+const memory = new Memory
+
+class Array {
+    constructor() {
+        this.length = 0;
+        this._capacity = 0;
+        this.ptr = memory.allocate(this.length);
+    }
+    
+    push(value) {
+        //check if we need to increase our capacity
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO)
+        }
+        //set the value at the memory location that is this.length ahead of this.ptr
+        memory.set(this.ptr + this.length, value)
+        //represent this increase in our length property
+        this.length++;
+    }
+
+    _resize(size) {
+        const oldPtr = this.ptr;
+        this.ptr = memory.allocate(size)
+        if (this.ptr === null) {
+            throw new Error('Out of memory')
+        }
+        memory.copy(this.ptr, oldPtr, this.length)
+        memory.free(oldPtr)
+        this._capacity = size;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length)
+            throw new Error('Index error')
+        return memory.get(this.ptr+index)
+    }
+
+    pop() {
+        if (this.length === 0) {
+            throw new Error('Index error')
+        }
+        const value = memory.get(this.ptr + this.length-1)
+        this.length--;
+        return value;
+    }
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error')
+        }
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1)*Array.SIZE_RATIO)
+        }
+        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index)
+        memory.set(this.ptr + index, value);
+        this.length++;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error')
+        }
+        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1)
+        this.length--;
+
+    }
+}
+
+function main() {
+    Array.SIZE_RATIO = 3;
+    let arr = new Array();
+    arr.push('tauhida');
+    console.log(arr)
+    console.log(arr.get(0))
+}
+
+main();
---
+++ arrayExercises.ts
@@ -0,0 +1,104 @@
+const removeSpace = (str:string) =>  {
+    const res = []
+    str.split('').map(val => {
+        if (val === ' ')
+            res.push('%20')
+        else
+            res.push(val) 
+    })
+    return res.join('');
+}
+//console.log(removeSpace('Hey this is Peter'));
+//—————————————————————————————————————————————————————————————————————————————————————————
+//Remove all numbers lower than a given value
+const removeNum = (list:any[], num:any) => {
+    const res = []
+    list.forEach(val => {
+        if (val >= num)
+            res.push(val);
+    })
+    return res;
+}
+//—————————————————————————————————————————————————————————————————————————————————————————
+//Find the largest sum derived from a sequence of numbers 
+const maxSum = (list:number[]) => {
+    const relMax = [];
+    list.reduce((acc, curr) => {
+        relMax.push(acc+curr)
+        return acc+curr
+    })
+    return relMax.sort((a,b) => b-a)[0];
+}
+
+const arr = [4, 6, -3, 5, -2, 1];
+//console.log(maxSum(arr));
+//—————————————————————————————————————————————————————————————————————————————————————————
+//Remove a string of letters from another string (sentence)
+const removeChar = (sentence:string, chars:string) => {
+    const sentenceArr = [];
+    const charArr = [];
+    for (let i = 0; i < sentence.length; i++)
+        sentenceArr.push(sentence[i])
+    for (let i = 0; i < chars.length; i++)
+        charArr.push(chars[i])
+
+    sentenceArr.forEach((letter, i) => {
+        charArr.forEach(char => {
+            if (letter === char)
+                sentenceArr.splice(i, 1)
+        })
+    })
+    //concatenate the array of string chars
+    return sentenceArr.reduce((acc, curr) => acc+curr)
+
+}
+//console.log(removeChar('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'))
+//—————————————————————————————————————————————————————————————————————————————————————————
+
+//Given a list of numbers, each index returns the product of all of the other values except the number at the current index
+const product = (arr:number[]) => {
+    return arr.map((val, i) => {
+        return arr.slice(0,i).concat(arr.slice(i+1, arr.length)).reduce((acc, curr) => acc * curr)
+    })
+}
+
+//console.log(product([1, 3, 9, 4]));
+//—————————————————————————————————————————————————————————————————————————————————————————
+
+//If there is a zero, make the whole col and row 0s.
+const writeZero = (matrix:number[][]) => {
+    //Alvaro, why does this work? What is a better way to do this?
+    const originalArr = JSON.parse(JSON.stringify(matrix))
+    let arr = matrix;
+    arr.map((row, rowI) => {
+        row.map((col, colI) => {
+            if (col === 0) {
+                arr[rowI].forEach((num, i) => arr[rowI][i] = 0)
+                arr.forEach(newRow => newRow[colI] = 0)
+            }
+        })
+    })
+    return arr;
+}
+const matrix = (
+       [[1, 0, 1, 1, 0],
+        [0, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1]]
+)
+//console.log(writeZero(matrix));
+//—————————————————————————————————————————————————————————————————————————————————————————
+
+//See if two strings are rotations of one another
+const stringRotate = (str1:string, str2:string) => {
+    const temp = str1 + str1;
+    if (str1.length !== str2.length)
+        return false;
+    else if (temp.includes(str2))
+        return true;
+    else
+        return false
+}
+//console.log(stringRotate('amazon', 'azonma')) //false
+//console.log(stringRotate('amazon', 'azonam'))  //true
